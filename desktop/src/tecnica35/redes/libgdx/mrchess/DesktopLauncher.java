2cbdca34f424ef15075e9d4203ad4da8b8777028
Marino <marijmotta@gmail.com>
2022-10-04 01:22:47 -0300

    First commit

---
+++ desktop/src/tecnica35/redes/libgdx/mrchess/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package tecnica35.redes.libgdx.mrchess;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import tecnica35.redes.libgdx.mrchess.Mrchess;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("MrChess");
+		new Lwjgl3Application(new Mrchess(), config);
+	}
+}
